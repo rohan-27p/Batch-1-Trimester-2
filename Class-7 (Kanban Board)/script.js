9d3e4a7f2bc1dcb1395d226481916908ae10fee8
mrinal1224 <iammrinalkc@gmail.com>
2025-01-07 10:35:28 +0530

    tikcets generated wth task and id

--- Class-7 (Kanban Board)/script.js
+++ Class-7 (Kanban Board)/script.js
@@ -1,10 +1,11 @@
 const addBtn = document.querySelector(".add-btn");
 const modalCont = document.querySelector(".modal-cont");
-const taskArea = document.querySelector('.textArea-cont')
+const taskArea = document.querySelector(".textArea-cont");
+const mainCont = document.querySelector('.main-cont')
 
 let addBtnFlag = false;
 
-// Modal pop up Event 
+// Modal pop up Event
 
 addBtn.addEventListener("click", function () {
   addBtnFlag = !addBtnFlag;
@@ -15,13 +16,33 @@ addBtn.addEventListener("click", function () {
   }
 });
 
+// Create a ticket
+
+function createTicket(ticketTask , ticketId) {
+  let ticketCont = document.createElement("div");
+  ticketCont.setAttribute("class", "ticket-cont");
+  ticketCont.innerHTML = `<div class="ticket-cont">
+             <div class="ticket-color"></div>
+             <div class="ticket-id">${ticketId}</div>
+             <div class="task-area">${ticketTask}</div>
+              <div class="ticket-lock">
+                <i class="fa-solid fa-lock"></i>
+              </div>`;
+
+              mainCont.appendChild(ticketCont)
+}
+
 // add a task
 
-modalCont.addEventListener('keydown' , function(e){
-   if(e.key ==='Shift'){
-      const task = taskArea.value
-      const id = shortid()
-      console.log(task +  " -> " +  id)
-
-   }
-})
+modalCont.addEventListener("keydown", function (e) {
+  if (e.key === "Shift") {
+    const task = taskArea.value;
+    const id = shortid();
+    console.log(task + " -> " + id);
+    createTicket(task , id)
+    modalCont.style.display = 'none'
+    taskArea.value = ''
+    addBtnFlag = false
+    
+  }
+});

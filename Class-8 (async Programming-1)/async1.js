27877d1b769b48e4ba468b9a4ea095a4b457565a
mrinal1224 <iammrinalkc@gmail.com>
2025-01-21 11:49:02 +0530

    push

--- Class-8 (async Programming-1)/async1.js
+++ Class-8 (async Programming-1)/async1.js
@@ -4,9 +4,14 @@ function test1() {
   console.log("I am test 1");
 }
 
+function test3() {
+  console.log("I am test 3");
+}
+
 // test1()
 
- setTimeout(test1, 2000);
+setTimeout(test1, 2000);
+setTimeout(test3, 2000);
 
 function test2() {
   console.log("I am test 2");

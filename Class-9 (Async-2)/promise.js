96f07d4f5783ae3888aae04dfdd5d5c705ff6a76
mrinal1224 <iammrinalkc@gmail.com>
2025-01-22 11:39:11 +0530

    push

--- Class-9 (Async-2)/promise.js
+++ Class-9 (Async-2)/promise.js
@@ -1,18 +1,52 @@
-const p1 = new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    let isHeads = Math.random() > 0.5;
-    if (isHeads) {
-      resolve("Heads , Won the Toss");
-    } else {
-      reject("Tails , Lost the Toss");
-    }
-  }, 2000);
-});
+const p1 = function () {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isHeads = Math.random() >0;
+      if (isHeads) {
+        resolve("Heads , Won the Toss 1");
+      } else {
+        reject("Tails , Lost the Toss 1");
+      }
+    }, 2000);
+  });
+};
 
-p1.then(function (resolveData) {
+const p2 = function () {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isHeads = Math.random() > 0;
+      if (isHeads) {
+        resolve("Heads , Won the Toss 2");
+      } else {
+        reject("Tails , Lost the Toss 2");
+      }
+    }, 2000);
+  });
+};
+const p3 = function () {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isHeads = Math.random() > 0;
+      if (isHeads) {
+        resolve("Heads , Won the Toss 3");
+      } else {
+        reject("Tails , Lost the Toss 3");
+      }
+    }, 2000);
+  });
+};
+
+p1().then(function (resolveData) {
   console.log(resolveData);
-});
-
-p1.catch(function (rejectData) {
-  console.log(rejectData);
-});
+  return p2()
+}).then(function(resolveDataP2){
+    console.log(resolveDataP2)
+    return p3()
+}).then(function(resolveDataP3){
+    console.log(resolveDataP3)
+}).catch(function (rejectData) {
+    console.log("Catch-> " + rejectData);
+  })
+  .finally(function () {
+    console.log("promise settled");
+  });
